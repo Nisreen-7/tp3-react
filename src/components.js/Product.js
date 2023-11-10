aa24d5dc760a34ae3bf60e8c443b5414048426e6
nisreen khattam <NISOKH7GMAIL.COM>
2023-11-10 22:50:42 +0100

    commit test

--- src/components.js/Product.js
+++ src/components.js/Product.js
@@ -1,20 +1,26 @@
-import './Product.css';
+import "./Product.css";
 
 function Product(props) {
-return(
-    <div className='card' > 
-        
-        <img src= {props.product.image} alt=''/>
-        <h1 className='card-title'> {props.product.title} </h1>
-        <p className='card-description'> Description :{props.product.description}</p>
-        <p className='card-price'>  
-        <button className='btnn'>Acheter</button> Price: {props.product.price} €
-        </p>
+  return (
+    <div className="card">
+      <img src={props.product.image} alt="" />
+      <h1 className="card-title"> {props.product.title} </h1>
+      <p className="card-description">
+        {" "}
+        Description :{props.product.description}
+      </p>
+      <p className="card-price">
+        <button className="btnn">Acheter</button> Price: {props.product.price} €
+      </p>
 
-        <p className='card-category'><mark>Category :</mark>{props.product.category}</p>
-        <p className='card-rating'>Rating :{props.product.rating.rate}, Count: {props.product.rating.count} </p>
+      <p className="card-category">
+        <mark>Category :</mark>
+        {props.product.category}
+      </p>
+      <p className="card-rating">
+        Rating :{props.product.rating.rate}, Count: {props.product.rating.count}{" "}
+      </p>
     </div>
-)
-
+  );
 }
-export default Product; 
+export default Product;
